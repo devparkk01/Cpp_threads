ffaa2044733c8a10490fc0f2453a7d87ce4886e7
devparkk01 <dev.parkk01@gmail.com>
2024-11-17 16:47:47 +0530

    Adds unique_lock

--- mutex/mutex3.cpp
+++ mutex/mutex3.cpp
@@ -7,3 +7,35 @@
 #include<mutex>
 
 
+using namespace std; 
+
+
+mutex m;
+int counter = 0 ;
+
+
+void increment(int id) {
+    unique_lock<mutex> lock(m);
+    cout << "Thread id " << id << " entered the function " << '\n';
+    for(int i = 0 ; i < 5 ; ++i) {
+        counter++; 
+    }
+    cout << "Thread id " << id << " exiting the function " << '\n';
+    lock.unlock();
+}
+
+
+int main () {
+
+    vector<thread> t(5);
+    for(int i= 0 ; i < 5 ; ++i) {
+        t[i] = thread(increment, i);
+    }
+
+    for(int i = 0 ; i < 5; ++i) {
+        t[i].join(); 
+    }
+
+    return 0 ; 
+
+}
